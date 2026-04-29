4805baaee2f2179e9759a8f2c7088cf80dc5e114
gleb <116607327+loeclos@users.noreply.github.com>
2026-04-29 10:59:22 -0700

    fix alpha nvim

--- config/plugins/ui/alpha.nix
+++ config/plugins/ui/alpha.nix
@@ -3,68 +3,68 @@
   plugins.alpha = {
     enable = true;
     theme = "dashboard";
-  #
-  #   layout = [
-  #     {
-  #       type = "padding";
-  #       val = 2;
-  #     }
-  #     {
-  #       opts = {
-  #         hl = "Type";
-  #         position = "center";
-  #       };
-  #       type = "text";
-  #       val = [
-  #         "                                                                       "
-  #         "                                                                     "
-  #         "       ████ ██████           █████      ██                     "
-  #         "      ███████████             █████                             "
-  #         "      █████████ ███████████████████ ███   ███████████   "
-  #         "     █████████  ███    █████████████ █████ ██████████████   "
-  #         "    █████████ ██████████ █████████ █████ █████ ████ █████   "
-  #         "  ███████████ ███    ███ █████████ █████ █████ ████ █████  "
-  #         " ██████  █████████████████████ ████ █████ █████ ████ ██████ "
-  #         "                                                                       "
-  #       ];
-  #     }
-  #     {
-  #       type = "padding";
-  #       val = 2;
-  #     }
-  #     {
-  #       type = "group";
-  #       val = [
-  #         {
-  #           on_press = lib.nixvim.mkRaw "function() vim.cmd[[ene]] end";
-  #           opts = {
-  #             shortcut = "n";
-  #           };
-  #           type = "button";
-  #           val = "  New file";
-  #         }
-  #         {
-  #           on_press = lib.nixvim.mkRaw "function() vim.cmd[[qa]] end";
-  #           opts = {
-  #             shortcut = "q";
-  #           };
-  #           type = "button";
-  #           val = " Quit Neovim";
-  #         }
-  #       ];
-  #     }
-  #     {
-  #       type = "padding";
-  #       val = 2;
-  #     }
-  #     {
-  #       opts = {
-  #         hl = "Keyword";
-  #         position = "center";
-  #       };
-  #       type = "text";
-  #       val = "Inspiring quote here.";
-  #     }
-  #   ];
-  # };
+    #
+    #   layout = [
+    #     {
+    #       type = "padding";
+    #       val = 2;
+    #     }
+    #     {
+    #       opts = {
+    #         hl = "Type";
+    #         position = "center";
+    #       };
+    #       type = "text";
+    #       val = [
+    #         "                                                                       "
+    #         "                                                                     "
+    #         "       ████ ██████           █████      ██                     "
+    #         "      ███████████             █████                             "
+    #         "      █████████ ███████████████████ ███   ███████████   "
+    #         "     █████████  ███    █████████████ █████ ██████████████   "
+    #         "    █████████ ██████████ █████████ █████ █████ ████ █████   "
+    #         "  ███████████ ███    ███ █████████ █████ █████ ████ █████  "
+    #         " ██████  █████████████████████ ████ █████ █████ ████ ██████ "
+    #         "                                                                       "
+    #       ];
+    #     }
+    #     {
+    #       type = "padding";
+    #       val = 2;
+    #     }
+    #     {
+    #       type = "group";
+    #       val = [
+    #         {
+    #           on_press = lib.nixvim.mkRaw "function() vim.cmd[[ene]] end";
+    #           opts = {
+    #             shortcut = "n";
+    #           };
+    #           type = "button";
+    #           val = "  New file";
+    #         }
+    #         {
+    #           on_press = lib.nixvim.mkRaw "function() vim.cmd[[qa]] end";
+    #           opts = {
+    #             shortcut = "q";
+    #           };
+    #           type = "button";
+    #           val = " Quit Neovim";
+    #         }
+    #       ];
+    #     }
+    #     {
+    #       type = "padding";
+    #       val = 2;
+    #     }
+    #     {
+    #       opts = {
+    #         hl = "Keyword";
+    #         position = "center";
+    #       };
+    #       type = "text";
+    #       val = "Inspiring quote here.";
+    #     }
+    #   ];
+  };
 }
