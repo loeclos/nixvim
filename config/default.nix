f09e79032dfd1ac2fd1c855079405f93f44ec203
gleb <116607327+loeclos@users.noreply.github.com>
2026-04-29 10:53:36 -0700

    fix alpha nvim

--- config/default.nix
+++ config/default.nix
@@ -32,7 +32,7 @@ _: {
     # UI plugins
     ./plugins/ui/bufferline.nix
     ./plugins/ui/lualine.nix
-    ./plugins/ui/startup.nix
+    ./plugins/ui/alpha.nix
 
     # LSP and formatting
     ./plugins/lsp/lsp.nix
