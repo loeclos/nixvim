f1738e60d4c4d5c0e5339437134ff8991756e51f
gleb <116607327+loeclos@users.noreply.github.com>
2026-08-17 20:16:03 -0700

    feat: add better rust support

--- config/plugins/lsp/rust.nix
+++ config/plugins/lsp/rust.nix
@@ -3,6 +3,29 @@
   plugins = {
     rustaceanvim = {
       enable = true;
+      settings = {
+        server = {
+          default_settings = {
+            rust-analyzer = {
+              check = {
+                command = "clippy";
+              };
+              cargo = {
+                allFeatures = true;
+                buildScripts = {
+                  enable = true;
+                };
+              };
+              procMacro = {
+                enable = true;
+              };
+              diagnostics = {
+                enable = true;
+              };
+            };
+          };
+        };
+      };
     };
     crates = {
       enable = true;
